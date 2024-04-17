30e1b7ac1fd8f6ba2ffd8020ca8061df26024b27
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-04-17 16:38:46 +0530

    teacher dashboard and student dashboard was created

---
+++ student_enroll.js
@@ -0,0 +1,8 @@
+// Function to enroll in a course
+function enroll(courseName) {
+    // Here you can implement the logic to enroll the student in the selected course
+    console.log(`Enrolled in course: ${courseName}`);
+    // You can send requests to the server to enroll the student in the course
+    // For example, you can make an AJAX request to a backend endpoint to handle enrollment
+  }
+  
